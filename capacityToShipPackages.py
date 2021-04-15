94993b11a31f83ec30c33d020d728a9e1b160275
Brandon Dayauon <brdayauon@gmail.com>
2021-04-14 18:34:43 -0700

    added TC SC

--- capacityToShipPackages.py
+++ capacityToShipPackages.py
@@ -1,3 +1,11 @@
+# Time Complexity : O(nlg(sum(weights)- max(weight)))
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+
 class Solution:
     def shipWithinDays(self, weights: List[int], D: int) -> int:
         left = max(weights)
