4dd18a9a770bd2e309d005432229a6835b5f536b
Nayana S <nayasrin@cisco.com>
2023-04-02 22:07:50 -0700

    Completed Misc-3

---
+++ problem_2.cpp
@@ -0,0 +1,85 @@
+25. Reverse Nodes in k-Group
+
+TC O(n)
+SC O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(k<=1)
+        return head;
+        ListNode *t=head;
+        ListNode *p=head->next;
+        ListNode *x=head;
+        ListNode *tmp=head;
+        ListNode *dmy=new ListNode;
+        int n,c=0,f=0;
+        while(t){
+                t=t->next;
+                c++;
+            }
+        t=head;
+        n=c/k;
+// pointing pointer x to the node where the previous node to be added.
+        for(int i=0;i<n;i++){
+            int j=0;
+            while(j<k){ 
+                if(x->next==NULL){
+                    dmy->val=x->val;
+                        x->next=dmy;
+                        x=x->next;
+                        f=1;
+                        break;
+                    }
+                     x=x->next;
+                    j++;
+                }
+//adding the nodes
+                int l=0;
+                while(l<k){
+                    t->next=x;
+                    x=t;
+                    t=p;
+                    p=p->next;
+                l++;
+                }
+                if(i==0){              //maintaining head position, only in 
+                head=x;                // first iteration.
+                tmp=x;
+                }
+                else{                 // linking the previous nodes.
+                    int y=0; 
+                    while(y<(k-1)){
+                        tmp=tmp->next;
+                        y++;
+                        }
+                    tmp->next=x;
+                    tmp=x;
+                }
+                x=t;
+        }
+
+        while(tmp){
+            if(tmp->next==NULL)
+                return head;
+                
+            else if(tmp->next==dmy){
+                tmp->next=NULL;
+                return head;
+                }
+            tmp=tmp->next;
+            
+        }
+    return head;
+    }
+};
